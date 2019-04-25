409d359cedd2d4ea03c60fe4452c96081b4bcc3e
Shubham Ojha <shubh1795@gmail.com>
2019-04-26 00:31:18 +0530

    Array Manipulation Partial Submission

---
+++ IPK/ArrayManipulation.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    arr = [0] * (n+1)
+    for i in queries:
+        for j in range(i[0],i[1]+1):
+            arr[j] += i[2]
+    arr.sort()
+    return arr[-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
